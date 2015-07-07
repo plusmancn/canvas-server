fd418bf804b2128483095bf5238f3506817e29c6
root <plusmancn@gmail.com>
2015-07-07 15:04:30 +0800

    字体v1版本

--- server.js
+++ server.js
@@ -5,17 +5,27 @@ var avatarGenerateGm = require('./lib/avatar_generate_gm.js');
 
 var server = http.createServer(function(req,res){
 
-  if(/\/CharacterImage\/([^\/]+)/.test(req.url)){
+  if(/\/CharacterImage\/([^\/]+)$/.test(req.url)){
     var character = decodeURIComponent(RegExp.$1);
     res.writeHead(200,{"Content-Type":"application/json; charset=utf-8"});
 
     avatarGenerateGm.getImageUrl(character,function(err,data){
-
+      var result = {};
+      if(err){
+        result.code = err.code || 1;
+        result.message = err.message || err.error || 'unknown error';
+      }else{
+        result.code = 0;
+        result.message = 'success';
+        result.data = data;
+      }
+      res.end(JSON.stringify(result));
     });
+  }else{
+    res.writeHead(301,{'Location':'https://github.com/plusmancn/canvas-server'});
+    res.end();
   }
 
-  res.writeHead(301,{'Location':'https://github.com/plusmancn/canvas-server'});
-  res.end();
 });
 
 server.listen(8001);
