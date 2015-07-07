37dac961c807a0e26942987639c0e7aa2bcb8880
root <plusmancn@gmail.com>
2015-07-07 14:08:24 +0800

    修改

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 'use strict';
-// 开放图片生成服务
+var url = require('url');
+var http = require('http');
+var avatarGenerateGm = require('./lib/avatar_generate_gm.js');
 
+var server = http.createServer(function(req,res){
+
+  if(/\/CharacterImage\/([^\/]+)$/.test(req.url)){
+    var character = decodeURIComponent(RegExp.$1);
+    res.writeHead(200,{"Content-Type":"application/json; charset=utf-8"});
+
+
+    avatarGenerateGm.getImageUrl(character,function(err,data){
+
+    });
+  }
+
+  res.write(301,{'Location':'https://github.com/plusmancn/canvas-server'});
+});
+
+server.listen(8001);
+console.log('server listen on 8001',Date.now());
